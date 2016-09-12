472c7c95125fceef6b79278419897e1e16f4de4c
Matthew Condit <mcondit@toxikon.com>
2016-09-12 15:01:44 -0400

    Chose background image, added my own nav bar

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express boy!' });
+  res.render('index', { title: 'Matthew Condit:  Web Developer' });
 });
 
 module.exports = router;
