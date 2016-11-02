82f4a55b77d517f621de48627a643d60127c045e
Matthew Condit <mcondit@toxikon.com>
2016-11-02 16:31:31 -0400

    Fixed scrolling bug on blog page

--- public/javascripts/blog/blog.js
+++ public/javascripts/blog/blog.js
@@ -4,87 +4,51 @@ var app = angular.module('blogApp', []);
 
 var singleBlogApp = angular.module('singleBlogApp', ['ngRoute']);
 
-app.controller('mainCtrl',function($scope, $http, $sce, $filter) {
+app.controller('mainCtrl', function ($scope, $http, $sce, $filter) {
   console.log('normal app controller');
-  $scope.refresh = function(callback) {
-    $http.get('api/blogs').then(function(response) {
+  $scope.refresh = function (callback) {
+    $http.get('api/blogs').then(function (response) {
       for (var i in response.data.blogs) {
         response.data.blogs[i].preview = $sce.trustAsHtml($filter('limitTo')(response.data.blogs[i].body, 300) + '...');
-      }; 
+      };
       $scope.blogs = response.data.blogs;
       callback();
-  })};
-  $scope.refresh(function() {
-    var waitlist = $('.waiting');
-    console.log(waitlist.length);
-    waitlist.each(function(i, el) {
-      console.log('Not sure why this isnt firing');
-      var $el = waitlist.eq(i);
-      if ($el.visible(true)) {
-        $el.addClass("already-visible").removeClass('waiting'); 
-      } 
-    }) 
-  }); 
-  
-  $scope.delete = function ( blog ) {
-    $http.delete('../api/blog/' + blog._id).then(function(response) {
-      $scope.refresh(function() {});
+    })
+  };
+  $scope.refresh(function () {
+    $http.get('api/blogs').then(function (response) {
+      $scope.blogs = response.data.blogs;
+    })
+  });
+
+  $scope.delete = function (blog) {
+    $http.delete('../api/blog/' + blog._id).then(function (response) {
+      $scope.refresh(function () {});
     });
   };
 
-  $scope.like = function ( blog ) {
-    $http.post('../api/like/' + blog._id).then(function(response){
+  $scope.like = function (blog) {
+    $http.post('../api/like/' + blog._id).then(function (response) {
       blog.liked = true;
-      $scope.refresh(function() {});
+      $scope.refresh(function () {});
     });
   };
-  
-  
-  
-  var win = $(window); 
-  win.scroll(function(event) {
-    var waitlist = $('.waiting');
-    console.log(waitlist.length);
-    waitlist.each(function(i, el) {
-      var $el = waitlist.eq(i);
-      if ($el.visible(true)) {
-        $el.addClass("come-in").removeClass('waiting'); 
-      } 
-    }); 
-  });
 });
 
 
 
-singleBlogApp.controller('blogCtrl',function($scope, $http, $location, $sce) {
+singleBlogApp.controller('blogCtrl', function ($scope, $http, $location, $sce) {
   console.log('Single app controller');
   var path = $location.absUrl().split(/[\s/]+/).pop();
   console.log(path);
   console.dir($location);
-  $http.get('../api/blog/' + path).then(function(response) {
+  $http.get('../api/blog/' + path).then(function (response) {
     console.dir(response.data);
     $scope.blog = response.data.blog;
-    $scope.blogHTML = $sce.trustAsHtml('<p>' +response.data.blog.body +'</p>');
+    $scope.blogHTML = $sce.trustAsHtml('<p>' + response.data.blog.body + '</p>');
     $scope.author = response.data.author[0];
   });
 
 
 
 });
-
-(function($) {
-
-  $.fn.visible = function(partial) {  
-    var $t            = $(this),
-        $w            = $(window),
-        viewTop       = $w.scrollTop(),
-        viewBottom    = viewTop + $w.height(),
-        _top          = $t.offset().top,
-        _bottom       = _top + $t.height(),
-        compareTop    = partial === true ? _bottom : _top,
-        compareBottom = partial === true ? _top : _bottom;
-    if ($t.offset().top < $w.height()) return false;
-    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
-  };
-    
-})(jQuery);
